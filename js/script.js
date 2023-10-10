4c78844b24bfe20dc6c1cd272cb41ff39ec6d222
giuliabaratella <giuliabaratella7@gmail.com>
2023-10-10 16:25:35 +0200

    added const for passenger name and nKm

--- js/script.js
+++ js/script.js
@@ -3,9 +3,24 @@ const discountUnder18 = 20 / 100;
 const discountOver65 = 40 / 100;
 // console.log(priceKm, discountUnder18, discountOver65);
 
-const btnSubmit = document.getElementById(submit);
-const btnReset = document.getElementById(reset);
+const btnSubmit = document.getElementById('submit');
+const btnReset = document.getElementById('reset');
 console.log (submit, reset);
 
+btnSubmit.addEventListener('click', 
+function() {
+    const passengerName = document.getElementById('name-surname').value;
+    const nKm = parseInt(document.getElementById('nKm').value);
+
+    if (nKm === ''|| passengerName === '') {
+        alert('Compila tutti i campi')
+    } else if (isNaN(nKm)) {
+        alert ('Inserisci un valore numerico per i km')
+    } else {
+        console.log (passengerName, nKm);
+    }
+
+}
+)
 
 
