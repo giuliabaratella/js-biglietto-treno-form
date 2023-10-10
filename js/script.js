c1a5c7b5b195c0cb30e32ff5da7785fbf94228f8
giuliabaratella <giuliabaratella7@gmail.com>
2023-10-10 15:40:47 +0200

    added const priceKm, discounts, buttons

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const priceKm = 0.21;
+const discountUnder18 = 20 / 100;
+const discountOver65 = 40 / 100;
+// console.log(priceKm, discountUnder18, discountOver65);
+
+const btnSubmit = document.getElementById(submit);
+const btnReset = document.getElementById(reset);
+console.log (submit, reset);
+
+
+
